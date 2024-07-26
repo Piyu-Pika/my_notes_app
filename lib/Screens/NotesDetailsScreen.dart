e8a59257b5f4b4db5a4973796e53b45937e35bab
Piyush Bhardwaj <piyushbhardwj1603@gmail.com>
2024-07-26 12:35:43 +0530

    modified ui

--- lib/Screens/NotesDetailsScreen.dart
+++ lib/Screens/NotesDetailsScreen.dart
@@ -5,6 +5,7 @@ import 'package:intl/intl.dart';
 import 'package:my_notes_app/Screens/ChatWithNotes.dart';
 import 'package:my_notes_app/Screens/NoteEditScreen.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:share_plus/share_plus.dart';
 
 class NoteDetailScreen extends StatefulWidget {
   final Map<String, dynamic> note;
@@ -18,6 +19,15 @@ class NoteDetailScreen extends StatefulWidget {
 
 class _NoteDetailScreenState extends State<NoteDetailScreen> {
   late Map<String, dynamic> _note;
+
+  final List<Color> _colorOptions = [
+    Colors.white,
+    Colors.red[100]!,
+    Colors.blue[100]!,
+    Colors.green[100]!,
+    Colors.yellow[100]!,
+    Colors.purple[100]!,
+  ];
 
   @override
   void initState() {
@@ -61,16 +71,65 @@ class _NoteDetailScreenState extends State<NoteDetailScreen> {
     );
   }
 
-  Color getTextColor(Color backgroundColor) {
-    return backgroundColor.computeLuminance() > 0.5
-        ? Colors.black
-        : Colors.white;
+  void _shareNote() {
+    Share.share('${_note['title']}\n\n${_note['content']}');
+  }
+
+  void _changeColor() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Pick a color'),
+          content: SingleChildScrollView(
+            child: Wrap(
+              spacing: 8,
+              runSpacing: 8,
+              children: _colorOptions.map((Color color) {
+                return GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _note['color'] = color.value;
+                    });
+                    Navigator.of(context).pop();
+                    _updateNoteColor();
+                  },
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: color,
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.grey),
+                    ),
+                    child: color.value == _note['color']
+                        ? Icon(Icons.check, color: Colors.black)
+                        : null,
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _updateNoteColor() {
+    final user = FirebaseAuth.instance.currentUser!;
+    FirebaseFirestore.instance
+        .collection('users')
+        .doc(user.uid)
+        .collection('notes')
+        .doc(widget.noteId)
+        .update({'color': _note['color']});
   }
 
   @override
   Widget build(BuildContext context) {
     Color noteColor = Color(_note['color'] ?? Colors.white.value);
-    Color textColor = getTextColor(noteColor);
+    Color textColor =
+        noteColor.computeLuminance() > 0.5 ? Colors.black : Colors.white;
 
     return Scaffold(
       appBar: AppBar(
@@ -80,6 +139,8 @@ class _NoteDetailScreenState extends State<NoteDetailScreen> {
               icon: Icon(Icons.edit), onPressed: () => _editNote(context)),
           IconButton(
               icon: Icon(Icons.delete), onPressed: () => _deleteNote(context)),
+          IconButton(icon: Icon(Icons.share), onPressed: _shareNote),
+          IconButton(icon: Icon(Icons.color_lens), onPressed: _changeColor),
         ],
       ),
       body: SingleChildScrollView(
@@ -109,6 +170,7 @@ class _NoteDetailScreenState extends State<NoteDetailScreen> {
     return Card(
       elevation: 4,
       color: noteColor,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
